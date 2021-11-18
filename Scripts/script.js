8239bc57d350c0195ddbadb699df8442295506c3
DevelopynWesley <wesley.Barrett@developyn.com>
2021-11-18 09:09:10 +0000

    Format changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,38 +2,38 @@ function appendList() {
 
     let currList = document.getElementById('objList');
     let newText = document.getElementById('newText').value;
-
     let newLItem = document.createElement('li');
+
     newLItem.appendChild(document.createTextNode(newText));
     currList.appendChild(newLItem);
-
+    
     let delBtn = document.createElement("button");
-    delBtn.innerHTML = "X"
+    delBtn.innerHTML = "X";
+    newLItem.id = document.getElementById("objList").getElementsByTagName("li").length + 1;
+    delBtn.onclick = function () {document.getElementById(newLItem.id).remove();}
     
-    
-    newLItem.id = document.getElementById("objList").getElementsByTagName("li").length + 1
-     delBtn.onclick = function () {document.getElementById(newLItem.id).remove();}
-     newLItem.appendChild(delBtn);
-     console.log('appended');
+    newLItem.appendChild(delBtn); 
 
 }
 
 function deleteItem() {
 
-    let index = document.getElementById('delNum').value
-    let currList = document.getElementById('objList')
-    currList.removeChild(currList.childNodes[index])
+    let index = document.getElementById('delNum').value;
+    let currList = document.getElementById('objList');
+    currList.removeChild(currList.childNodes[index]);
     
     let content = currList.innerHTML;
     currList.innerHTML= content; 
 
 }
 
-function checkEnter(e)
-{
+function checkEnter(e){
+
     let key=e.keyCode || e.which;
-  if (key==13){
-     appendList();
-  }
+      
+    if (key==13)
+    {
+      appendList();
+    }
 
 }
